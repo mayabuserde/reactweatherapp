daf6eb6241723ca8970b94e5de60b2aa176a1930
Maya Buser De <maya.buser@gmail.com>
2021-06-30 10:27:23 -0400

    take care of warnings in deployment

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,23 +1,23 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { faCloudSun } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from "axios";
+// import axios from "axios";
 
 
 export default function CurrentWeather() {
-   let [city, setCity] = useState('');
-    function showWeather(response) {
-       alert(response.data.name)
-   }
+  //  let [city, setCity] = useState('');
+  //   function showWeather(response) {
+  //      alert(response.data.name)
+  //  }
 
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=$Atlanta&units=metric&appid=ffa66c6f94c53b49251ff2fd2a6dec80`;
+  //   let url = `https://api.openweathermap.org/data/2.5/weather?q=$Atlanta&units=metric&appid=ffa66c6f94c53b49251ff2fd2a6dec80`;
   
-    axios.get(url).then(showWeather);
+  //   axios.get(url).then(showWeather);
   
     return (
         <div className="CurrentWeather row mb-5">
           <div className="col-8">
-            <h2>{city}</h2>
+            <h2>Atlanta, GA</h2>
             <h4>Sunday, June 13 2021</h4>
             <p>Partly Cloudy</p>
             <FontAwesomeIcon icon={faCloudSun} />

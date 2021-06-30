fff4b4df4bc71234e954ddfbb4ffea575002ef5a
Maya Buser De <maya.buser@gmail.com>
2021-06-30 10:38:09 -0400

    Adding links to repo and portfolio

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import Forecast from "./Forecast";
 
 export default function App() {
   return (
-    <div className="App container border rounded my-5">
-      <Search />
-      <CurrentWeather />
-      <Forecast />
+    <div className="container">
+      <div className="App container border rounded my-5">
+        <Search />
+        <CurrentWeather />
+        <Forecast />
+      </div>
+      <footer><a href="https://github.com/mayabuserde/reactweatherapp" target="_blank" rel="noreferrer">Open-source code</a>, by <a href="https://mayabuserde.github.io/" target="_blank" rel="noreferrer">Maya Buser De</a></footer>
     </div>
   );
 }
